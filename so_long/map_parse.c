212b49c6933b98e908a90a5d0c8ebe5676ef53ec
Domenico Musomecci <dmusomec@c1r5p11.42firenze.it>
2025-03-13 11:51:35 +0100

    fanculo

--- so_long/map_parse.c
+++ so_long/map_parse.c
@@ -6,16 +6,29 @@
 /*   By: dmusomec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 13:22:08 by dmusomec          #+#    #+#             */
-/*   Updated: 2025/03/12 17:18:39 by dmusomec         ###   ########.fr       */
+/*   Updated: 2025/03/12 19:11:01 by dmusomec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	check_line(char *line)
+int	check_line(t_data *game)
 {
-	if (ft_strchr_str(line, "01CEP") == -1)
+	if (ft_strchr_str(game->line, "01CEP\n") == -1)
 		return (-1);
+	if (game->width == 0)
+		game->width = ft_strlen(game->line);
+	if (game->height == 0 && ft_strchr_str(game->line, "1\n") == -1)
+		return (-1);
+	if (game->line[0] != '1' || ft_strlen(game->line) < 2)
+		return (-1);
+	if (ft_strlen(game->line) != game->width)
+	{
+		if (game->line[game->width] == '\0' && ft_strchr_str(game->line, "1\n") != -1)
+			return (1);
+		else
+			return (-1);
+	}
 	else
 		return (1);
 }
@@ -25,11 +38,10 @@ int	get_size(t_data *game)
 	game->line = get_next_line(game->fd);
 	if (!game->line)
 		return (-1);
-	if (game->width == 0)
-		game->width = ft_strlen(game->line);
-	if (ft_strlen(game->line) != game->width || ft_strlen(game->line) < 2)
+	if (check_line(game) == -1)
 		return (-1);
 	game->height++;
+	return (1);
 }
 
 int	main()
@@ -37,20 +49,15 @@ int	main()
 	t_data *game = malloc(sizeof(t_data) * 1);
 	if (!game)
 		return (1);
-	int i = 0;
 	game->map = 0;
 	game->line = "0";
+	game->height = 0;
 	game->fd = 0;
 	game->i = 0;
 	game->fd = open("texto.txt", O_RDONLY);
-	if (check_line("000111l") == -1)
-		printf("mala\n");
-	if (check_line("0000111PPPEEE") == 1)
-		printf("buena\n");
 	while (get_size(game) != -1)
 	{
 		printf("%s", game->line);
-
 	}
 	close(game->fd);
 	return 0;
