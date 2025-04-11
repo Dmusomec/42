08752064c21208e159cc427ccb7587244c50f2a1
dmusomec <musomeccidomenico@gmail.com>
2025-04-12 01:29:18 +0200

    huh

--- minitalktry1/client.c
+++ minitalktry1/client.c
@@ -6,59 +6,49 @@
 /*   By: dmusomec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 21:19:32 by dmusomec          #+#    #+#             */
-/*   Updated: 2025/04/05 21:31:57 by dmusomec         ###   ########.fr       */
+/*   Updated: 2025/04/12 01:25:38 by dmusomec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include "libft/libft.h"
+#include "minitalk.h"
 
-static void	send_char(int pid, char c)
+void	put_signal(int pid, char *str)
 {
 	int	i;
+	int	c;
 
-	i = 8;
-	while (i--)
+	i = 0;
+	while (*str)
 	{
-		if ((c >> i) & 1)
+		c = *(str);
+		while (i < 8)
 		{
-			if (kill(pid, SIGUSR2) == -1)
-			{
-				ft_putstr_fd("Error: kill()\n", STDERR_FILENO);
-				exit(EXIT_FAILURE);
-			}
+			if (c << i & 0b10000000)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			i++;
+			usleep(200);
 		}
-		else
-		{
-			if (kill(pid, SIGUSR1) == -1)
-			{
-				ft_putstr_fd("Error: kill()\n", STDERR_FILENO);
-				exit(EXIT_FAILURE);
-			}
-		}
-		usleep(300);
+		str++;
+		i = 0;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
-
-	if (argc != 3 || !ft_strlen(argv[2]))
+	if (ft_atoi(argv[1]) <= 0)
 	{
-		ft_putstr_fd("Usage: ./client [PID] [MESSAGE]\n", STDERR_FILENO);
-		exit(EXIT_FAILURE);
+		ft_printf("Invalid PID.\n");
+		exit(1);
 	}
-	pid = ft_atoi(argv[1]);
-	if (pid <= 0 || kill(pid, 0) == -1)
+	if (argc == 3)
+		put_signal(ft_atoi(argv[1]), argv[2]);
+	else
 	{
-		ft_putstr_fd("Error: Invalid PID\n", STDERR_FILENO);
-		exit(EXIT_FAILURE);
+		ft_printf("Invalid arguments, Put PID and messange.\n");
+		exit(1);
 	}
-	while (*argv[2])
-		send_char(pid, *argv[2]++);
-	send_char(pid, '\0');
-	return (EXIT_SUCCESS);
+	return (0);
 }
--- minitalktry1/minitalk.h
+++ minitalktry1/minitalk.h
@@ -1,42 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmusomec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/05 21:18:11 by dmusomec          #+#    #+#             */
-/*   Updated: 2025/04/12 01:25:50 by dmusomec         ###   ########.fr       */
+/*   Created: 2025/04/12 01:25:08 by dmusomec          #+#    #+#             */
+/*   Updated: 2025/04/12 01:25:10 by dmusomec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#ifndef MINITALK_H
+# define MINITALK_H
 
-void	take_signal(int sig)
-{
-	static int	c;
-	static int	i;
+# include <bits/types/siginfo_t.h>
+# include <signal.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "libft/libft.h"
 
-	if (sig == SIGUSR2)
-		c = c << 1;
-	else if (sig == SIGUSR1)
-		c = c << 1 | 0b00000001;
-	i++;
-	if (i == 8)
-	{
-		ft_printf("%c", c);
-		i = 0;
-		c = 0;
-	}
-}
-
-int	main(void)
-{
-	ft_printf("PID process: %d\n", getpid());
-	while (1)
-	{
-		signal(SIGUSR1, take_signal);
-		signal(SIGUSR2, take_signal);
-	}
-	return (0);
-}
+#endif
