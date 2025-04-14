ee9e237e5c5ec15652ec13ba14b35e4a8a3116b7
Domenico Musomecci <dmusomec@c1r5p10.42firenze.it>
2025-04-14 18:46:06 +0200

    huhuhuhu

--- minitalktry1/client.c
+++ minitalktry1/client.c
@@ -6,7 +6,7 @@
 /*   By: dmusomec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 21:19:32 by dmusomec          #+#    #+#             */
-/*   Updated: 2025/04/12 01:25:38 by dmusomec         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:25:04 by dmusomec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,38 @@ void	put_signal(int pid, char *str)
 	int	i;
 	int	c;
 
-	i = 0;
+	i = 7;
 	while (*str)
 	{
 		c = *(str);
-		while (i < 8)
+		i = 7;
+		while (i >= 0)
 		{
-			if (c << i & 0b10000000)
+			if ((c >> i) & 1)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
-			i++;
 			usleep(200);
+			i--;
 		}
 		str++;
-		i = 0;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	if (ft_atoi(argv[1]) <= 0)
-	{
-		ft_printf("Invalid PID.\n");
-		exit(1);
-	}
 	if (argc == 3)
+	{
+		if (ft_atoi(argv[1]) <= 0)
+		{
+			ft_printf("Invalid PID.\n");
+			exit(1);
+		}
 		put_signal(ft_atoi(argv[1]), argv[2]);
+	}
 	else
 	{
-		ft_printf("Invalid arguments, Put PID and messange.\n");
+		ft_printf("Invalid arguments. Example: ./client [PID] [String]\n");
 		exit(1);
 	}
 	return (0);
--- minitalktry1/server.c
+++ minitalktry1/server.c
@@ -6,7 +6,7 @@
 /*   By: dmusomec <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 21:18:11 by dmusomec          #+#    #+#             */
-/*   Updated: 2025/04/12 01:25:50 by dmusomec         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:11:00 by dmusomec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	take_signal(int sig)
 	if (sig == SIGUSR2)
 		c = c << 1;
 	else if (sig == SIGUSR1)
-		c = c << 1 | 0b00000001;
+		c = c << 1 | 1;
 	i++;
 	if (i == 8)
 	{
@@ -32,7 +32,7 @@ void	take_signal(int sig)
 
 int	main(void)
 {
-	ft_printf("PID process: %d\n", getpid());
+	ft_printf("Process ID: %d\n", getpid());
 	while (1)
 	{
 		signal(SIGUSR1, take_signal);
